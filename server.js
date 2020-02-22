77d8acba9e13765162d069788cd4e27ed0c739a8
James Goff <james_goff88@outlook.com>
2020-02-22 13:19:26 -0600

    update to work on heroku.

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const mongoose = require('mongoose');
-  const routes = require('./routes/api');
-  const path = require('path');
-  require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
+require('dotenv').config();
 
-  const app = express();
+const app = express();
 
-  const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-  //connect to the database
-  mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true
-      })
-      .then(() => console.log(`Database connected successfully`))
-      .catch(err => console.log(err));
+//connect to the database
+mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log(`Database connected successfully`))
+    .catch(err => console.log(err));
 
-  //since mongoose promise is depreciated, we overide it with node's promise
-  mongoose.Promise = global.Promise;
+//since mongoose promise is depreciated, we overide it with node's promise
+mongoose.Promise = global.Promise;
 
-  app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      next();
-  });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-  app.use('/api', routes);
+app.use('/api', routes);
 
-  app.use((err, req, res, next) => {
-      console.log(err);
-      next();
-  });
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+});
 
-  app.listen(port, () => {
-      console.log(`Server running on port ${port}`)
-  });
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
